6d58f0c600132c35469171ebe3c4f2b8eb0e39b1
macbookpro <abenov2005.uzer@gmail.com>
2020-10-16 23:32:17 +0600

    renamed -> MainFragment.kt to TopHeadlinesFragment.kt

---
+++ app/src/main/java/com/alis/news/ui/top_headlines/TopHeadlinesFragment.kt
@@ -0,0 +1,163 @@
+package com.alis.news.ui.top_headlines
+
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.alis.news.R
+import com.alis.news.adapters.NewsAdapter
+import com.alis.news.data.remote.Status
+import com.alis.news.extension.invisible
+import com.alis.news.extension.isNetworkAvailable
+import com.alis.news.extension.showToastShort
+import com.alis.news.extension.visible
+import com.alis.news.models.NewsArticles
+import com.alis.news.ui.details.DetailsFragment
+import com.alis.news.utils.SimpleOnQueryTextListener
+import kotlinx.android.synthetic.main.fragment_top_headlines.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class TopHeadlinesFragment : Fragment() {
+
+    private val viewModel by viewModel<TopHeadlinesViewModel>()
+
+    private lateinit var adapterTopHeadlines: NewsAdapter
+    private var listTopHeadlines: MutableList<NewsArticles> = mutableListOf()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_top_headlines, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initializeViews()
+        setUpListeners()
+        observe()
+    }
+
+    private fun initializeViews() {
+        adapterTopHeadlines = NewsAdapter(listTopHeadlines)
+        val layoutManagerTopHeadlines = LinearLayoutManager(context)
+        recycler_top_headlines.apply {
+            addItemDecoration(
+                DividerItemDecoration(
+                    context,
+                    layoutManagerTopHeadlines.orientation
+                )
+            )
+            layoutManager = layoutManagerTopHeadlines
+            adapter = adapterTopHeadlines
+        }
+    }
+
+    private fun setUpListeners() {
+        clickAdapter()
+        addOnScrollRecycler()
+    }
+
+    private fun clickAdapter() {
+        adapterTopHeadlines.setOnItemClickListener(object : NewsAdapter.OnItemClickListener {
+            override fun onNewsItemClick(item: NewsArticles) {
+                DetailsFragment.start(
+                    requireActivity(),
+                    R.id.action_navigation_top_headlines_to_detailsFragment,
+                    item
+                )
+            }
+        })
+    }
+
+    private fun addOnScrollRecycler() {
+        recycler_top_headlines.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1)) {
+                    viewModel.fetchTopHeadlinesFromAPI()
+                }
+            }
+        })
+    }
+
+    private fun observe() {
+        fetchTopHeadlines()
+
+        subscribeToNews()
+        subscribeToNewsQuery()
+    }
+
+    private fun fetchTopHeadlines() {
+        if (isNetworkAvailable(requireActivity())) {
+            viewModel.fetchTopHeadlinesFromAPI()
+        } else {
+            viewModel.fetchTopHeadlinesFromDatabase()
+        }
+    }
+
+    private fun subscribeToNews() {
+        viewModel.news.observe(viewLifecycleOwner, {
+            val articles = it.data?.articles
+            when (it.status) {
+                Status.LOADING -> progress_top_headlines.visible()
+                Status.SUCCESS -> {
+                    progress_top_headlines.invisible()
+                    if (articles != null) {
+                        adapterTopHeadlines.addAll(articles)
+                    }
+                }
+            }
+        })
+    }
+
+    private fun subscribeToNewsQuery() {
+        viewModel.newsQuery.observe(viewLifecycleOwner, {
+            val articles = it.data?.articles
+            when (it.status) {
+                Status.LOADING -> progress_top_headlines.visible()
+                Status.SUCCESS -> {
+                    progress_top_headlines.invisible()
+                    listTopHeadlines.clear()
+                    if (articles != null) {
+                        adapterTopHeadlines.addAll(articles)
+                    }
+                }
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_toolbar_main, menu)
+        searchLogic(menu)
+        clearLogic(menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    private fun searchLogic(menu: Menu) {
+        val itemSearch = menu.findItem(R.id.search_menu)
+        val searchView: SearchView = itemSearch.actionView as SearchView
+        searchView.setOnQueryTextListener(
+            object : SimpleOnQueryTextListener(), OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    viewModel.fetchTopHeadlinesQuery(query.toString())
+                    return true
+                }
+            })
+    }
+
+    private fun clearLogic(menu: Menu) {
+        val itemClear = menu.findItem(R.id.clear_all_saved)
+        itemClear.setOnMenuItemClickListener {
+            viewModel.clearDatabase()
+            showToastShort(requireContext(), R.string.toast_cache_cleared)
+            true
+        }
+    }
+}
--- app/src/main/java/com/alis/news/ui/top_headlines/TopHeadlinesViewModel.kt
+++ app/src/main/java/com/alis/news/ui/top_headlines/TopHeadlinesViewModel.kt
@@ -2,21 +2,18 @@ package com.alis.news.ui.top_headlines
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.alis.news.data.network.Resource
+import com.alis.news.data.remote.Resource
 import com.alis.news.data.repository.NewsRepository
 import com.alis.news.models.NewsResponse
 
 class TopHeadlinesViewModel(private val newsRepository: NewsRepository) : ViewModel() {
 
     var news = MutableLiveData<Resource<NewsResponse>>()
+    var newsQuery = MutableLiveData<Resource<NewsResponse>>()
 
-    private var page: Int = 1
+    private var page: Int = 0
 
-    init {
-        fetchTopHeadlines()
-    }
-
-    private fun fetchTopHeadlines() {
+    fun fetchTopHeadlinesFromAPI() {
         page += 1
         news = newsRepository.fetchTopHeadlines(
             "us",
@@ -25,98 +22,17 @@ class TopHeadlinesViewModel(private val newsRepository: NewsRepository) : ViewMo
         ) as MutableLiveData<Resource<NewsResponse>>
     }
 
-    /* var news: MutableLiveData<List<NewsArticles>> = MutableLiveData()
-     var searchNews: MutableLiveData<List<NewsArticles>> = MutableLiveData()
-     var isEndpoint: MutableLiveData<Boolean> = MutableLiveData()
+    fun fetchTopHeadlinesFromDatabase() {
+        //TODO: fetch news from db
+    }
 
-     private var mNews: List<NewsArticles>? = null
-     private var page: Int = 0
+    fun fetchTopHeadlinesQuery(q: String) {
+        newsQuery = newsRepository.fetchTopHeadlinesQuery(
+            q
+        ) as MutableLiveData<Resource<NewsResponse>>
+    }
 
-     fun requestToAPI() {
-         incrementPage()
-         if (App.preference?.endpoint()!!) {
-             App.newsRepository?.getTopHeadlines(
-                 "us",
-                 10,
-                 page,
-                 object : NewsAPIClient.NewsActionCallback {
-
-                     override fun onSuccess(result: NewsResponse) {
-                         Log.d("topHeadlinesRequest", result.articles.toString())
-                         mNews = result.articles
-                         news.value = result.articles
-                     }
-
-                     override fun onFailure(exception: Exception) {
-                         Log.d("topHeadlinesRequest", exception.toString())
-                     }
-                 }
-             )
-         } else {
-             App.newsRepository?.getEverything(
-                 "us",
-                 10,
-                 page,
-                 object : NewsAPIClient.NewsActionCallback {
-
-                     override fun onSuccess(result: NewsResponse) {
-                         Log.d("everythingRequest", result.articles.toString())
-                         mNews = result.articles
-                         news.value = result.articles
-                     }
-
-                     override fun onFailure(exception: Exception) {
-                         Log.d("everythingRequest", exception.toString())
-                     }
-                 }
-             )
-         }
-     }
-
-     private fun incrementPage() {
-         page += 1
-     }
-
-     fun requestToDatabase() {
-         news.value = App.newsDatabase!!.newsDao().getAll()
-     }
-
-     fun searchRequestToAPI(q: String?) {
-         App.newsRepository?.getActionWithSearch(
-             q,
-             object : NewsAPIClient.NewsActionCallback {
-
-                 override fun onSuccess(result: NewsResponse) {
-                     Log.d("searchRequestToApi", result.articles.toString())
-                     searchNews.value = result.articles
-                     insertInDatabase(result)
-                 }
-
-                 override fun onFailure(exception: Exception) {
-                     Log.d("searchRequestToApi", exception.toString())
-                 }
-             }
-         )
-     }
-
-     private fun insertInDatabase(result: NewsResponse) {
-         App.newsDatabase!!.newsDao().insert(result.articles)
-     }
-
-     fun clickTopHeadlines() {
-         page = 0
-         isEndpoint.value = true
-         App.preference?.setEndpoint(true)
-     }
-
-     fun clickEverything() {
-         page = 0
-         isEndpoint.value = false
-         App.preference?.setEndpoint(false)
-     }
-
-     fun clickClear() {
-         App.newsDatabase!!.newsDao().deleteAll()
-         news.value = null
-     }*/
+    fun clearDatabase() {
+        //TODO: clear db
+    }
 }
