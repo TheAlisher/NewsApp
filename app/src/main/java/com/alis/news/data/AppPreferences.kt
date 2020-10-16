1cc2dac43227fec947623f31395d508892ace883
macbookpro <abenov2005.uzer@gmail.com>
2020-10-17 01:02:12 +0600

    renamed -> Preference key

--- app/src/main/java/com/alis/news/data/AppPreferences.kt
+++ app/src/main/java/com/alis/news/data/AppPreferences.kt
@@ -7,5 +7,5 @@ import com.alis.news.App
 class AppPreferences(context: Context) {
 
     private var preferences: SharedPreferences =
-        context.getSharedPreferences("lotcion_preferences", Context.MODE_PRIVATE)
+        context.getSharedPreferences("news_preferences", Context.MODE_PRIVATE)
 }
