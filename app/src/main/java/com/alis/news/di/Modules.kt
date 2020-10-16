35f02597a5b18a1338ceedb2ba1c0537d062ed26
macbookpro <abenov2005.uzer@gmail.com>
2020-10-17 01:12:31 +0600

    added -> DatabaseClient

--- app/src/main/java/com/alis/news/di/Modules.kt
+++ app/src/main/java/com/alis/news/di/Modules.kt
@@ -1,10 +1,12 @@
 package com.alis.news.di
 
 import com.alis.news.data.AppPreferences
+import com.alis.news.data.local.db.DatabaseClient
 import com.alis.news.data.remote.RetrofitClient
 import com.alis.news.data.repository.NewsRepository
 import com.alis.news.ui.everything.EverythingViewModel
 import com.alis.news.ui.top_headlines.TopHeadlinesViewModel
+import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -12,8 +14,11 @@ var newsModule = module {
 
     single { AppPreferences(get()) }
     single { RetrofitClient().provideNews() }
+    single { DatabaseClient().provideDatabase(androidApplication()) }
+    single { DatabaseClient().provideNews(get()) }
 
     factory { NewsRepository(get()) }
+
 
     viewModel { TopHeadlinesViewModel(get()) }
     viewModel { EverythingViewModel(get()) }
