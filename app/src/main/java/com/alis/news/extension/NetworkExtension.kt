37de2af99ecc2921797a9c19338cb29fa593209d
macbookpro <abenov2005.uzer@gmail.com>
2020-10-17 01:53:33 +0600

    refactor

--- app/src/main/java/com/alis/news/extension/NetworkExtension.kt
+++ app/src/main/java/com/alis/news/extension/NetworkExtension.kt
@@ -3,10 +3,10 @@ package com.alis.news.extension
 import android.app.Activity
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkInfo
 
-fun isNetworkAvailable(activity: Activity): Boolean {
-    val connectivityManager =
-        activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-    val activeNetworkInfo = connectivityManager!!.activeNetworkInfo
-    return activeNetworkInfo != null && activeNetworkInfo.isConnected
+fun isOnline(activity: Activity): Boolean {
+    val connectivityManager = activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+    return networkInfo?.isConnected == true
 }
