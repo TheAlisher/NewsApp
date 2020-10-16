7872ee84f23b44525447efdc5618d0c267ae3635
macbookpro <abenov2005.uzer@gmail.com>
2020-10-17 00:59:24 +0600

    removed unnecessary

--- app/src/main/java/com/alis/news/data/AppPreferences.kt
+++ app/src/main/java/com/alis/news/data/AppPreferences.kt
@@ -4,18 +4,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.alis.news.App
 
-private const val PREF_ENDPOINTS = "endpoints"
-
 class AppPreferences(context: Context) {
 
     private var preferences: SharedPreferences =
         context.getSharedPreferences("lotcion_preferences", Context.MODE_PRIVATE)
-
-    fun endpoint(): Boolean? {
-        return preferences.getBoolean(PREF_ENDPOINTS, true)
-    }
-
-    fun setEndpoint(endpoint: Boolean) {
-        preferences.edit().putBoolean(PREF_ENDPOINTS, endpoint).apply()
-    }
 }
