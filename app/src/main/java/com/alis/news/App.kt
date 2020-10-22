4f0ff11951f9f5bd4f824b927123bf898724c0de
macbookpro <abenov2005.uzer@gmail.com>
2020-10-23 00:24:39 +0600

    removed unnecessary

--- app/src/main/java/com/alis/news/App.kt
+++ app/src/main/java/com/alis/news/App.kt
@@ -1,8 +1,6 @@
 package com.alis.news
 
 import android.app.Application
-import androidx.room.Room
-import com.alis.news.data.local.db.NewsDatabase
 import com.alis.news.di.newsModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
